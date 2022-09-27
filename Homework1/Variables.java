87a6691c1fd79786812cc6c2969ef1a3f0b5ccd1
OzlemSevinc <114358268+OzlemSevinc@users.noreply.github.com>
2022-09-28 00:52:42 +0300

    Add files via upload

---
+++ Homework1/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        int ogrenciSayisi = 11;
+        String mesaj = "Öğrenci sayım :";
+        System.out.println(mesaj+ogrenciSayisi);
+        System.out.println(mesaj+ogrenciSayisi);
+        System.out.println("Öğrenci sayım :"+ogrenciSayisi);
+        System.out.println("Öğrenci sayım :"+ogrenciSayisi);
+        System.out.println("Öğrenci sayım :"+ogrenciSayisi);
+    }
+}
